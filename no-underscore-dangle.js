c4a8d86a2ab4e84d62c8e159016859d118bc1293
will-obrien <will.obrien@villagechief.com>
2018-06-19 17:13:35 +1000

    ewsdfsdf

--- no-underscore-dangle.js
+++ no-underscore-dangle.js
@@ -9,3 +9,28 @@ var _ = require('underscore');
 var obj = _.contains(items, item);
 obj.__proto__ = {};
 var file = __filename;
+
+//correct for allowAfterThis true
+var a = this.foo_;
+this._bar();
+
+//correct for allowAfterSuper true
+var a = super.foo_;
+super._bar();
+
+//incorrect for enforceinmethodnames true
+class Foo {
+  _bar() {}
+}
+
+class Foo {
+  bar_() {}
+}
+
+const o = {
+  _bar() {}
+};
+
+const o = {
+  bar_() = {}
+};
