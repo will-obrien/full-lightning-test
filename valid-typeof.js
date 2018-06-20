b776a7f54236b086ea9b1a09bf45359a0c7183e0
will-obrien <will.obrien@villagechief.com>
2018-06-20 15:46:16 +1000

    xxx

--- valid-typeof.js
+++ valid-typeof.js
@@ -1,25 +1,25 @@
 //incorrect
-typeof foo === "strnig"
-typeof foo == "undefimed"
-typeof bar != "nunber"
-typeof bar !== "function"
+typeof foo === "strnig";
+typeof foo == "undefimed";
+typeof bar != "nunber";
+typeof bar !== "function";
 
 //correct
-typeof foo === "string"
-typeof bar == "undefined"
-typeof foo === baz
-typeof bar === typeof qux
+typeof foo === "string";
+typeof bar == "undefined";
+typeof foo === baz;
+typeof bar === typeof qux;
 
 //incorrect with "requireStringLiterals": true
-typeof foo === undefined
-typeof bar == Object
-typeof baz === "strnig"
-typeof qux === "some invalid type"
-typeof baz === anotherVariable
-typeof foo == 5
+typeof foo === undefined;
+typeof bar == Object;
+typeof baz === "strnig";
+typeof qux === "some invalid type";
+typeof baz === anotherVariable;
+typeof foo == 5;
 
 //correct with "requireStringLiterals": true
-typeof foo === "undefined"
-typeof bar == "object"
-typeof baz === "string"
-typeof bar === typeof qux
+typeof foo === "undefined";
+typeof bar == "object";
+typeof baz === "string";
+typeof bar === typeof qux;
