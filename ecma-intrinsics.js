fcc357bd430c50acd43f1fc855c630c878b06e95
will-obrien <will.obrien@villagechief.com>
2018-06-22 10:38:51 +1000

    change

--- ecma-intrinsics.js
+++ ecma-intrinsics.js
@@ -1,3 +1,2 @@
-var regexObj = new RegExp('foo', 'gi'); 
-regexObj.compile('new foo', 'g')     //This is a violation, compile() is a deprecated method.
+
 String.doSomething = function(){}  //This is a violation, the intrinsic type String is modified.
